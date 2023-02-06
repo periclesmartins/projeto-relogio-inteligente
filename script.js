ca6ebef592915a09a8e5d6b48bd87a99d6171530
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-02-06 10:23:33 -0300

    Substituindo 'innerHTML' por 'textContent', que é mais seguro e rápido.

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ function carregar() {
     let minutos = data.getMinutes()
     let segundos = data.getSeconds()
     
-    msg.innerHTML = `Agora são ${hora} horas, ${minutos} minutos, ${segundos} segundos.`
+    msg.textContent = `Agora são ${hora} horas, ${minutos} minutos, ${segundos} segundos.`
     if (hora >= 5 && hora < 12) {
         // bom dia
         img.src = "imagens/manha.png"
         document.body.style.background = '#fcd896'
-        title.innerHTML = 'Está de manhã.'
+        title.textContent = 'Está de manhã.'
     } else if (hora >= 12 && hora <= 18) {
         // boa tarde
         img.src = "imagens/tarde.png"
         document.body.style.background = '#f7b02c'
-        title.innerHTML = 'Está de tarde.'
+        title.textContent = 'Está de tarde.'
     } else {
         // boa noite
         img.src = "imagens/noite.png"
         document.body.style.background = '#034c7c'
-        title.innerHTML = 'Está de noite.'
+        title.textContent = 'Está de noite.'
     } setTimeout(carregar, 1000)   
 }
 
